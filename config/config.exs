03914922baa92d0d22f47a9bc791d1304e98abdb
Jonathan Gao <gsmlg.com@gmail.com>
2022-04-26 18:05:03 +0800

    fix: Minify js.

--- config/config.exs
+++ config/config.exs
@@ -5,6 +5,6 @@ config :phoenix, :json_library, Jason
 config :esbuild,
   version: "0.14.0",
   default: [
-    args: ~w(priv/src/phoenix_webcomponent.js --bundle --target=esnext --outdir=priv/static/),
+    args: ~w(priv/src/phoenix_webcomponent.js --bundle --minify --target=esnext --outdir=priv/static/),
     cd: Path.expand("../", __DIR__)
   ]
