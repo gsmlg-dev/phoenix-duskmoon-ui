f50230a58c8d5bbaaed3baadf410eece21b12b8d
Jonathan Gao <gsmlg.com@gmail.com>
2022-09-24 08:20:26 +0800

    feat: Add wc_card.

---
+++ apps/phoenix_webcomponent/lib/phoenix_webcomponent/card.ex
@@ -0,0 +1,30 @@
+defmodule Phoenix.WebComponent.Card do
+  @moduledoc """
+  Render card.
+  """
+  use Phoenix.WebComponent, :component
+
+  # alias Phoenix.LiveView.JS
+
+  @doc """
+  Generates card
+  """
+  def wc_card(assigns) do
+    assigns =
+      assigns
+      |> assign_new(:id, fn -> false end)
+      |> assign_new(:title, fn -> nil end)
+      |> assign_new(:class, fn -> "" end)
+
+    ~H"""
+    <div id={@id} class={"bg-white w-auto h-fit m-4 p-6 flex flex-col shadow #{@class}"}>
+      <%= unless is_nil(@title) do %>
+      <div class="w-full text-xl flex flex-row justify-start items-center h-10 mb-4">
+        <%= render_slot(@title) %>
+      </div>
+      <% end %>
+      <%= render_slot(@inner_block) %>
+    </div>
+    """
+  end
+end
---
+++ apps/phx_wc_storybook_web/lib/phx_wc_storybook_web/storybook/components/card.exs
@@ -0,0 +1,24 @@
+defmodule PhxWCStorybookWeb.Storybook.Components.Card do
+  # :live_component or :page are also available
+  use PhxLiveStorybook.Entry, :component
+
+  def function, do: &Phoenix.WebComponent.Card.wc_card/1
+  def description, do: "A card element."
+
+  def stories do
+    [
+      %Story{
+        id: :default,
+        slots: ["""
+        <:title>
+        Star Wars
+        </:title>
+        Star Wars is an American epic space opera multimedia
+        franchise created by George Lucas,
+        which began with the eponymous 1977 film and
+        quickly became a worldwide pop-culture phenomenon.
+        """]
+      }
+    ]
+  end
+end
