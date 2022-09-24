689913cfe2e58f651c0902856115702f3475a960
Jonathan Gao <gsmlg.com@gmail.com>
2022-09-24 08:20:15 +0800

    fix: Fix wc_table.

--- apps/phoenix_webcomponent/lib/phoenix_webcomponent/table.ex
+++ apps/phoenix_webcomponent/lib/phoenix_webcomponent/table.ex
@@ -1,39 +1,36 @@
 defmodule Phoenix.WebComponent.Table do
   @moduledoc """
   Render table.
-
   """
   use Phoenix.WebComponent, :component
 
   @doc """
   Generates a table.
-
   ## Examples
-
   ## Options
-
   """
   def wc_table(assigns) do
     assigns =
       assigns
       |> assign_new(:id, fn -> false end)
+      |> assign_new(:class, fn -> "" end)
       |> assign_new(:col, fn -> [] end)
       |> assign_new(:rows, fn -> [] end)
 
     ~H"""
-    <table class="table-fixed border-collapse border-spacing-0 border border-slate-400">
+    <table id={@id} class={"table-fixed border-collapse border-spacing-0 #{@class}"}>
       <thead>
-        <tr>
+        <tr class="bg-slate-100 h-[34px]">
           <%= for col <- @col do %>
-            <th class="border border-slate-400 px-4 py-2 slate-700"><%= col.label %></th>
+            <th class=" px-4 py-2 slate-700 text-left font-medium"><%= col.label %></th>
           <% end %>
         </tr>
       </thead>
       <tbody>
       <%= for row <- @rows do %>
-        <tr>
+        <tr class="bg-slate-50 even:bg-white h-[40px]">
           <%= for col <- @col do %>
-            <td class="border border-slate-400 px-4 py-2"><%= render_slot(col, row) %></td>
+            <td class=" px-4 py-2"><%= render_slot(col, row) %></td>
           <% end %>
         </tr>
       <% end %>
