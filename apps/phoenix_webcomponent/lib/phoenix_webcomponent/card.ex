3585d8f047d90b81e50f338a2f2a3ed9ac287647
Jonathan Gao <gsmlg.com@gmail.com>
2022-09-28 20:43:35 +0800

    fix: Fix wc_card width and storybook.

--- apps/phoenix_webcomponent/lib/phoenix_webcomponent/card.ex
+++ apps/phoenix_webcomponent/lib/phoenix_webcomponent/card.ex
@@ -17,7 +17,7 @@ defmodule Phoenix.WebComponent.Card do
       |> assign_new(:title, fn -> nil end)
 
     ~H"""
-    <div id={@id} class={"bg-white w-auto h-fit m-4 p-6 flex flex-col shadow #{@class}"}>
+    <div id={@id} class={"bg-white w-fit h-fit m-4 p-6 flex flex-col shadow #{@class}"}>
       <%= unless is_nil(@title) do %>
       <div class="w-full text-xl flex flex-row justify-start items-center h-10 mb-4">
         <%= render_slot(@title) %>
