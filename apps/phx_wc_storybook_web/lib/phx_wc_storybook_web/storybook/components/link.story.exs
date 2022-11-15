2234b3dbe84e0c99b48507a6c92c6acd3a1ae2ab
Jonathan Gao <gsmlg.com@gmail.com>
2022-11-15 23:10:18 +0800

    fix: Add new link storybbok.

---
+++ apps/phx_wc_storybook_web/lib/phx_wc_storybook_web/storybook/components/link.story.exs
@@ -0,0 +1,47 @@
+defmodule PhxWCStorybookWeb.Storybook.Components.Link do
+  # :live_component or :page are also available
+  use PhxLiveStorybook.Story, :component
+
+  def function, do: &Phoenix.WebComponent.Link.wc_link/1
+  def description, do: "Recreate Phoenix.Component.link with custom element."
+
+  def variations do
+    [
+      %Variation{
+        id: :default,
+        attributes: %{
+          href: "/"
+        },
+        slots: [
+          """
+          Back to home
+          """
+        ]
+      },
+      %Variation{
+        id: :primary,
+        attributes: %{
+          href: "/",
+          mode: "primary"
+        },
+        slots: [
+          """
+          Back to home
+          """
+        ]
+      },
+      %Variation{
+        id: :round,
+        attributes: %{
+          href: "/",
+          mode: "round"
+        },
+        slots: [
+          """
+          Back to home
+          """
+        ]
+      },
+    ]
+  end
+end
