57a4045c82d28c850b2e60c2e176c4486a5c9ded
Jonathan Gao <gsmlg.com@gmail.com>
2023-01-26 01:51:44 +0800

    chore: Add new task to generate yew.rs component.

---
+++ apps/phoenix_webcomponent/lib/mix/tasks/gen_rs_icon.ex
@@ -0,0 +1,45 @@
+defmodule Mix.Tasks.PhxWc.GenRsIcons do
+  # Generate Material Design Icons and Bootstrap Icons to yew components
+
+  @moduledoc """
+  Generate mdi.rs or bsi.rs fron svg files.
+  """
+  use Mix.Task
+
+  @shortdoc "Generate mdi.rs or bsi.rs file"
+  def run(["mdi"]) do
+    Mix.Task.run("app.start")
+
+    icons =
+      File.ls!(Application.app_dir(:phoenix_webcomponent, "priv/mdi/svg"))
+      |> Enum.filter(&String.ends_with?(&1, ".svg"))
+      |> Enum.map(&String.trim(&1, ".svg"))
+
+    quoted = EEx.compile_file("#{__DIR__}/yew_mdi.eex")
+    {result, _bindings} = Code.eval_quoted(quoted, icons: icons)
+
+    fl = Path.expand("../../../mdi.rs", __DIR__)
+
+    File.write!(fl, result)
+  end
+
+  def run(["bsi"]) do
+    Mix.Task.run("app.start")
+
+    icons =
+      File.ls!(Application.app_dir(:phoenix_webcomponent, "priv/bsi/svg"))
+      |> Enum.filter(&String.ends_with?(&1, ".svg"))
+      |> Enum.map(&String.trim(&1, ".svg"))
+
+    quoted = EEx.compile_file("#{__DIR__}/yew_bsi.eex")
+    {result, _bindings} = Code.eval_quoted(quoted, icons: icons)
+
+    fl = Path.expand("../../../bsi.rs", __DIR__)
+
+    File.write!(fl, result)
+  end
+
+  def run(_) do
+    IO.puts("Use `mix phx_wc.gen_rs_icons bsi` or `mix phx_wc.gen_rs_icons mdi`")
+  end
+end
