4e839c0017c1af1f990ad5a9473b51c61b4dbe07
Jonathan Gao <gsmlg.com@gmail.com>
2024-08-09 10:11:00 +0800

    fix: Ignore warning as error.

--- apps/duskmoon_storybook_web/lib/duskmoon_storybook_web/storybook.ex
+++ apps/duskmoon_storybook_web/lib/duskmoon_storybook_web/storybook.ex
@@ -1,14 +1,8 @@
 defmodule DuskmoonStorybookWeb.Storybook do
-  @moduledoc false
   use PhoenixStorybook,
-    # OTP name of your application.
     otp_app: :duskmoon_storybook_web,
 
-    # Path to your storybook stories (required).
     content_path: Path.expand("storybook", __DIR__),
-
-    # Custom storybook title. Default is "Storybook".
-    title: "Duskmoon UI Storybook",
 
     # Story compilation mode, can be either `:eager` or `:lazy`.
     # It defaults to `:lazy` in dev environment, `:eager` in other environments.
