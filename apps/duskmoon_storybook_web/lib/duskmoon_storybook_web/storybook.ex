0a45248cfc0af465dd91882953b45f6e03686ccb
Jonathan Gao <gsmlg.com@gmail.com>
2025-02-04 21:53:24 +0800

    chore: Fix ci.

--- apps/duskmoon_storybook_web/lib/duskmoon_storybook_web/storybook.ex
+++ apps/duskmoon_storybook_web/lib/duskmoon_storybook_web/storybook.ex
@@ -3,17 +3,6 @@ defmodule DuskmoonStorybookWeb.Storybook do
     otp_app: :duskmoon_storybook_web,
     content_path: Path.expand("storybook", __DIR__),
 
-    # Path to your JS asset, which will be loaded just before PhoenixStorybook's own
-    # JS. It's mainly intended to define your LiveView Hooks in `window.storybook.Hooks`.
-    # Remote path (not local file-system path) which means this file should be served
-    # by your own application endpoint.
-    js_path: "/assets/storybook.js",
-
-    # Path to your components stylesheet.
-    # Remote path (not local file-system path) which means this file should be served
-    # by your own application endpoint.
-    css_path: "/assets/storybook.css",
-
     # Custom storybook title. Default is "Live Storybook".
     title: "Phoenix Duskmoon UI Storybook",
 
