cc3f899791ca20b7be501d3b1038094f950d85e7
Jonathan Gao <gsmlg.com@gmail.com>
2025-02-04 20:41:52 +0800

    fix: Fix.

--- apps/duskmoon_storybook_web/assets/js/app.js
+++ apps/duskmoon_storybook_web/assets/js/app.js
@@ -1,7 +1,7 @@
 import "phoenix_html";
-import { Socket } from "phoenix";
-import { LiveSocket } from "phoenix_live_view";
 import "@gsmlg/lit";
+// import { Socket } from "phoenix";
+// import { LiveSocket } from "phoenix_live_view";
 
 // const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
 // const liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken }, hooks: { WebComponentHook } });
