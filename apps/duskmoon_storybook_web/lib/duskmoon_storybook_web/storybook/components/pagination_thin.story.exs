492b810047db09e420b2e7fa80927e32b163b7b2
Jonathan Gao <gsmlg.com@gmail.com>
2025-02-04 18:37:43 +0800

    fix: Fix pagination.

---
+++ apps/duskmoon_storybook_web/lib/duskmoon_storybook_web/storybook/components/pagination_thin.story.exs
@@ -0,0 +1,31 @@
+defmodule DuskmoonStorybookWeb.Storybook.Components.PaginationThin do
+  # :live_component or :page are also available
+  use PhoenixStorybook.Story, :component
+
+  def function, do: &PhoenixDuskmoon.Component.Pagination.dm_pagination_thin/1
+  def description, do: "A pagination thin element."
+
+  def variations do
+    [
+      %Variation{
+        id: :default,
+        attributes: %{
+          total: 100,
+          page_num: 5,
+          page_size: 15,
+          update_event: "ignore"
+        }
+      },
+      %Variation{
+        id: :show_page_jumper,
+        attributes: %{
+          total: 10000,
+          page_num: 5,
+          page_size: 15,
+          show_page_jumper: true,
+          update_event: "ignore"
+        }
+      }
+    ]
+  end
+end
--- apps/phoenix_duskmoon/lib/phoenix_duskmoon/component/pagination.ex
+++ apps/phoenix_duskmoon/lib/phoenix_duskmoon/component/pagination.ex
@@ -5,6 +5,8 @@ defmodule PhoenixDuskmoon.Component.Pagination do
   """
   use PhoenixDuskmoon.Component, :html
 
+  import PhoenixDuskmoon.Component.Icons
+
   @doc """
   Generates a pagination.
 
@@ -56,39 +58,38 @@ defmodule PhoenixDuskmoon.Component.Pagination do
     """
   )
 
+  attr(:page_url, :any,
+    default: nil,
+    doc: """
+    Page url.
+    """
+  )
+
+  attr(:page_url_marker, :string,
+    default: "{page}",
+    doc: """
+    Marker in page url. Default is `{page}`.
+    Replace this marker with page number.
+    """
+  )
+
+  attr(:page_link_type, :string,
+    default: "patch",
+    values: ~w(patch navigate href),
+    doc: """
+    Phoenix link type.
+    """
+  )
+
+  slot(:inner_block,
+    required: false,
+    doc: """
+    Page slot
+    """
+  )
+
   def dm_pagination(assigns) do
-    max_page =
-      if assigns.total > 0 do
-        (assigns.total / assigns.page_size) |> ceil
-      else
-        1
-      end
-
-    page_num = assigns.page_num
-
-    pages =
-      cond do
-        max_page == 1 ->
-          [1]
-
-        max_page < 7 ->
-          1..max_page |> Enum.map(& &1)
-
-        page_num < 3 ->
-          [1, 2, 3] ++ ["..."] ++ [max_page - 2, max_page - 1, max_page]
-
-        page_num == 3 ->
-          [1, 2, 3, 4] ++ ["..."] ++ [max_page - 2, max_page - 1, max_page]
-
-        page_num > 3 && page_num < max_page - 2 ->
-          [1] ++ ["...", page_num - 1, page_num, page_num + 1, "..."] ++ [max_page]
-
-        page_num == max_page - 2 ->
-          [1, 2, 3] ++ ["...", max_page - 3, max_page - 2, max_page - 1, max_page]
-
-        page_num > max_page - 2 ->
-          [1, 2, 3] ++ ["...", max_page - 2, max_page - 1, max_page]
-      end
+    {max_page, pages} = generate_pages(assigns.total, assigns.page_size, assigns.page_num)
 
     assigns =
       assigns
@@ -99,87 +100,69 @@ defmodule PhoenixDuskmoon.Component.Pagination do
     <div
       id={@id}
       class={[
-        "flex items-center justify-end",
-        "border-t border-gray-200",
-        "bg-white px-4 py-3",
+        "flex items-center gap-4",
         @class
       ]}
     >
-      <div class="flex flex-row items-center mx-8">
-        <div class="inline-flex flex-row items-center mx-8">
-          <p class="inline-flex flex-row items-center text-medium text-gray-700">
-            <span class="mr-1 after:content-[':']">Total</span>
-            <span class="font-medium"><%= @total %></span>
-          </p>
-        </div>
-        <div>
-          <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-            <span
-              phx-click={if @page_num == 1, do: false, else: @update_event}
-              phx-value-current={if @page_num == 1, do: nil, else: @page_num - 1}
-              disabled={if @page_num == 1, do: true, else: false}
-              class={[
-                "relative inline-flex items-center",
-                "rounded-l-md border border-gray-300",
-                "bg-white hover:bg-gray-50 px-2 py-2",
-                "text-sm font-medium text-gray-500 focus:z-20",
-                if(@page_num == 1, do: "", else: "cursor-pointer"),
-              ]}>
-              <span class="sr-only">Previous</span>
-              <!-- Heroicon name: mini/chevron-left -->
-              <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clip-rule="evenodd" />
-              </svg>
-            </span>
-            <!-- Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" -->
-
-          <%= for p <- @pages do %>
-            <%= if is_binary(p) do %>
-              <span
-                class={[
-                  "relative inline-flex items-center",
-                  "border border-gray-300",
-                  "bg-white hover:bg-gray-50",
-                  "px-4 py-2",
-                  "text-sm font-medium text-gray-500 focus:z-20",
-                ]}
-              ><%= p %></span>
-            <% else %>
-              <span
-                phx-click={@update_event} phx-value-current={p}
-                aria-current={if p == @page_num, do: "page", else: false }
-                class={[
-                  "relative inline-flex items-center",
-                  "border border-gray-300",
-                  "bg-white hover:bg-gray-50 px-4 py-2",
-                  "text-sm font-medium text-gray-500 focus:z-20",
-                  if(p == @page_num, do: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", else: "cursor-pointer"),
-                ]}
-              ><%= p %></span>
-            <% end %>
-          <% end %>
-
-            <span
-              phx-click={if @page_num == @max_page, do: false, else: @update_event}
-              phx-value-current={if @page_num == @max_page, do: nil, else: @page_num + 1}
-              disabled={if @page_num == @max_page, do: true, else: false}
-              class={[
-                "inline-flex items-center",
-                "relative px-2 py-2",
-                "border border-gray-300 rounded-r-md",
-                "bg-white hover:bg-gray-50",
-                "text-sm font-medium text-gray-500 focus:z-20",
-                if(@page_num == @max_page, do: "", else: "cursor-pointer"),
-              ]}>
-              <span class="sr-only">Next</span>
-              <!-- Heroicon name: mini/chevron-right -->
-              <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
-              </svg>
-            </span>
-          </nav>
-        </div>
+      <div class="flex flex-row items-center gap-2">
+        <.dm_mdi name="view-dashboard" class="w-5 h-5" />
+        <code><%= @total %></code>
       </div>
+      <nav class="join" aria-label="Pagination">
+        <a
+          phx-click={if(@page_num == 1, do: false, else: @update_event)}
+          phx-value-current={if(@page_num == 1, do: nil, else: @page_num - 1)}
+          disabled={if(@page_num == 1, do: true, else: false)}
+          class={[
+            "join-item btn btn-sm",
+          ]}
+          data-phx-link={@page_link_type}
+          data-phx-link-state="push"
+          href={if(!is_nil(@page_url), do: String.replace(@page_url, @page_url_marker, "#{if(@page_num == 1, do: 1, else: @page_num - 1)}"), else: nil)}
+        >
+          <span class="sr-only">Previous</span>
+          <.dm_mdi name="page-previous" class="w-5 h-5" />
+        </a>
+
+        <%= for p <- @pages do %>
+          <%= if is_binary(p) do %>
+            <a
+              disabled="disabled"
+              class={[
+                "join-item btn btn-sm",
+              ]}
+            ><%= p %></a>
+          <% else %>
+            <a
+              phx-click={@update_event} phx-value-current={p}
+              aria-current={if p == @page_num, do: "page", else: false }
+              class={[
+                "join-item btn btn-sm",
+                if(p == @page_num, do: "btn-primary", else: ""),
+              ]}
+              data-phx-link={@page_link_type}
+              data-phx-link-state="push"
+              href={if(!is_nil(@page_url), do: String.replace(@page_url, @page_url_marker, "#{p}"), else: nil)}
+            ><%= p %></a>
+          <% end %>
+        <% end %>
+
+        <a
+          phx-click={if @page_num == @max_page, do: false, else: @update_event}
+          phx-value-current={if @page_num == @max_page, do: nil, else: @page_num + 1}
+          disabled={if @page_num == @max_page, do: true, else: false}
+          class={[
+            "join-item btn btn-sm",
+          ]}
+          data-phx-link={@page_link_type}
+          data-phx-link-state="push"
+          href={if(!is_nil(@page_url), do: String.replace(@page_url, @page_url_marker, "#{if @page_num == @max_page, do: @page_num, else: @page_num + 1}"), else: nil)}
+        >
+          <i class="sr-only">Next</i>
+          <.dm_mdi name="page-next" class="w-5 h-5" />
+        </a>
+      </nav>
+      <%= render_slot(@inner_block) %>
     </div>
     """
   end
@@ -254,18 +237,97 @@ defmodule PhoenixDuskmoon.Component.Pagination do
   )
 
   def dm_pagination_thin(assigns) do
+    {max_page, pages} = generate_pages(assigns.total, assigns.page_size, assigns.page_num)
+
     assigns =
       assigns
-      |> assign_new(:id, fn -> false end)
+      |> assign(:max_page, max_page)
+      |> assign(:pages, pages)
 
+    ~H"""
+    <div id={@id} class="flex items-center gap-4">
+      <div class="flex flex-row items-center gap-2">
+        <.dm_mdi name="view-dashboard" class="w-5 h-5" />
+        <code class="font-medium"><%= @total %></code>
+      </div>
+      <div>
+        <nav class="join" aria-label="Pagination">
+          <span
+            phx-click={if(@page_num == 1 || @loading, do: false, else: @update_event)}
+            phx-value-current={if @page_num == 1, do: nil, else: @page_num - 1}
+            disabled={if @page_num == 1, do: true, else: false}
+            class={[
+              if(@page_num == 1, do: "", else: "cursor-pointer"),
+              "join-item btn btn-sm",
+              if(@loading, do: "cursor-wait")
+            ]}
+          >
+            <span class="sr-only">Previous</span>
+            <.dm_mdi name="chevron-left" class="w-5 h-5" />
+          </span>
+
+          <%= for p <- @pages do %>
+            <%= if is_binary(p) do %>
+              <a disabled="disabled" class="join-item btn btn-sm"><%= p %></a>
+            <% else %>
+              <span
+                phx-click={if(@loading, do: false, else: @update_event)}
+                phx-value-current={p}
+                aria-current={if p == @page_num, do: "page", else: false}
+                class={[
+                  "join-item btn btn-sm",
+                  if(p == @page_num, do: " bg-primary text-primary-content", else: " cursor-pointer"),
+                  if(@loading, do: "cursor-wait")
+                ]}
+              >
+                <%= p %>
+              </span>
+            <% end %>
+          <% end %>
+
+          <span
+            phx-click={if(@page_num == @max_page || @loading, do: false, else: @update_event)}
+            phx-value-current={if @page_num == @max_page, do: nil, else: @page_num + 1}
+            disabled={if @page_num == @max_page, do: true, else: false}
+            class={[
+              if(@page_num == @max_page, do: "", else: "cursor-pointer"),
+              "join-item btn btn-sm",
+              if(@loading, do: "cursor-wait")
+            ]}
+          >
+            <span class="sr-only">Next</span>
+            <.dm_mdi name="chevron-right" class="w-5 h-5" />
+          </span>
+        </nav>
+      </div>
+      <div :if={@show_page_jumper}>
+        <div class="text-medium flex-nowrap flex items-center gap-2">
+          <span><.dm_mdi name="arrow-right-top" class="w-4 h-4" /></span>
+          <form class="font-medium" phx-change={if(@loading, do: false, else: @update_event)}>
+            <input
+              type="number"
+              name="current"
+              class={["input input-sm w-auto text-right"]}
+              min={1}
+              max={@max_page}
+              phx-debounce={300}
+              oninput={"this.value = Math.round(this.value);if(this.value<1){this.value=1}if(this.value>#{@max_page}){this.value=#{@max_page}}"}
+              value={@page_num}
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+    """
+  end
+
+  defp generate_pages(total, page_size, page_num) do
     max_page =
-      if assigns.total > 0 do
-        (assigns.total / assigns.page_size) |> ceil
+      if total > 0 do
+        (total / page_size) |> ceil
       else
         1
       end
-
-    page_num = assigns.page_num
 
     pages =
       cond do
@@ -291,115 +353,6 @@ defmodule PhoenixDuskmoon.Component.Pagination do
           [1, 2, 3] ++ ["...", max_page - 2, max_page - 1, max_page]
       end
 
-    assigns =
-      assigns
-      |> assign(:max_page, max_page)
-      |> assign(:pages, pages)
-
-    ~H"""
-    <div id={@id} class="flex items-center justify-end">
-      <div class="flex flex-row items-center justify-end flex-wrap-reverse gap-6">
-        <div class="flex flex-row items-center h-8">
-          <p class="text-medium text-gray-700 whitespace-nowrap">
-            <span class="mr-1 after:content-[':']">Total</span>
-            <span class="font-medium"><%= @total %></span>
-          </p>
-        </div>
-        <div>
-          <nav class="join" aria-label="Pagination">
-            <span
-              phx-click={if(@page_num == 1 || @loading, do: false, else: @update_event)}
-              phx-value-current={if @page_num == 1, do: nil, else: @page_num - 1}
-              disabled={if @page_num == 1, do: true, else: false}
-              class={[
-                if(@page_num == 1, do: "", else: "cursor-pointer"),
-                "join-item btn btn-sm",
-                if(@loading, do: "cursor-wait")
-              ]}
-            >
-              <span class="sr-only">Previous</span>
-              <!-- Heroicon name: mini/chevron-left -->
-              <svg
-                class="h-5 w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </span>
-
-            <%= for p <- @pages do %>
-              <%= if is_binary(p) do %>
-                <span class="join-item btn btn-sm"><%= p %></span>
-              <% else %>
-                <span
-                  phx-click={if(@loading, do: false, else: @update_event)}
-                  phx-value-current={p}
-                  aria-current={if p == @page_num, do: "page", else: false}
-                  class={[
-                    "join-item btn btn-sm",
-                    if(p == @page_num, do: " bg-primary text-primary-content", else: " cursor-pointer"),
-                    if(@loading, do: "cursor-wait")
-                  ]}
-                >
-                  <%= p %>
-                </span>
-              <% end %>
-            <% end %>
-
-            <span
-              phx-click={if(@page_num == @max_page || @loading, do: false, else: @update_event)}
-              phx-value-current={if @page_num == @max_page, do: nil, else: @page_num + 1}
-              disabled={if @page_num == @max_page, do: true, else: false}
-              class={[
-                if(@page_num == @max_page, do: "", else: "cursor-pointer"),
-                "join-item btn btn-sm",
-                if(@loading, do: "cursor-wait")
-              ]}
-            >
-              <span class="sr-only">Next</span>
-              <!-- Heroicon name: mini/chevron-right -->
-              <svg
-                class="h-5 w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </span>
-          </nav>
-        </div>
-        <div :if={@show_page_jumper}>
-          <div class="text-medium flex-nowrap flex items-center gap-2">
-            <span>Page</span>
-            <form class="font-medium" phx-change={if(@loading, do: false, else: @update_event)}>
-              <input
-                type="number"
-                name="current"
-                class={["input input-sm w-auto text-right"]}
-                min={1}
-                max={@max_page}
-                phx-debounce={300}
-                oninput={"this.value = Math.round(this.value);if(this.value<1){this.value=1}if(this.value>#{@max_page}){this.value=#{@max_page}}"}
-                value={@page_num}
-              />
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-    """
+    {max_page, pages}
   end
 end
