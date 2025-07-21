60be998f1ea056032cc020281d2dcba80d6e0676
Jonathan Gao <gsmlg.com@gmail.com>
2025-07-21 14:30:01 +0800

    fix: Fix.

--- apps/duskmoon_storybook_web/assets/js/app.js
+++ apps/duskmoon_storybook_web/assets/js/app.js
@@ -1,15 +1,1 @@
 import "phoenix_html";
-// import "@gsmlg/lit";
-// import { Socket } from "phoenix";
-// import { LiveSocket } from "phoenix_live_view";
-
-// const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
-// const liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken }, hooks: { WebComponentHook } });
-
-// liveSocket.connect();
-
-// // expose liveSocket on window for web console debug logs and latency simulation:
-// // >> liveSocket.enableDebug()
-// // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
-// // >> liveSocket.disableLatencySim()
-// window.liveSocket = liveSocket;
